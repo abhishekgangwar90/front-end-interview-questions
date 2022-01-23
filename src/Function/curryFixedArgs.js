c9574ff92d8800be84a61cd004063a60a6aafbdb
Abhishek Gangwar <me.abhishekgangwar@gmail.com>
2022-01-23 14:02:15 +0530

    feat: added fix for fixed curry

--- src/Function/curryFixedArgs.js
+++ src/Function/curryFixedArgs.js
@@ -6,11 +6,11 @@
 
 
 function add(maxArgsLength=3, ...args){
-  function returnFunc(...args2){
+  return function returnFunc(...args2){
     args = [...args, ...args2];
     if(args.length >= maxArgsLength){
       return args.slice(0,maxArgsLength).reduce((acc, curr) => acc + curr);
     }
+    return returnFunc
   }
-  return returnFunc()
 }
